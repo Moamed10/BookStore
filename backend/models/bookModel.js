f57f2ca6f415303038260c1e2a3d814220e777ad
Moamed10 <144000841+Moamed10@users.noreply.github.com>
2024-12-12 13:03:46 +0100

    book

---
+++ backend/models/bookModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const categories = [
+  "choose category", // This could be an optional placeholder value
+  "Business",
+  "Fiction",
+  "Horror",
+  "Adventure",
+];
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: categories, // Restrict category to the values in the categories array
+    },
+    trending: {
+      type: Boolean,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      required: true,
+    },
+    oldPrice: {
+      type: Number,
+      required: true,
+    },
+    newPrice: {
+      type: Number,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
