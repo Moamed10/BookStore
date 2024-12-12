2b6acbd82cfb5e572ba70f317915344c1dedd083
Wesam Shujaa <w.shujaa@presspage.com>
2024-12-12 15:06:14 +0100

    change roles to lowercase

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,28 +2,28 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const userSchema = new schema({
-  userName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
-  role: {
-    type: String,
-    enum: ["Reader", "Writer "],
-    default: "Reader",
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-  },
+    userName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    role: {
+        type: String,
+        enum: ["reader", "writer "],
+        default: "reader",
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model("user", userSchema);
