01f35146fe584dc2806bb2b858c9b49e72f1f05c
Moamed10 <144000841+Moamed10@users.noreply.github.com>
2024-12-12 16:47:06 +0100

    a

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.set("view engine", "ejs");
-
-// routes
-const bookRoutes = require("./src/books/book.route");
-const userRoutes = require("./src/users/user.route");
-
 let port = 3100;
 app.listen(port, () => {
   console.log(`app is on ${port}`);
