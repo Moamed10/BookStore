0856397a4567b9d16bb4486291a0e2f17283545d
Wesam Shujaa <w.shujaa@presspage.com>
2024-12-13 16:12:16 +0100

    Add .env and DB link

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const express = require("express");
-// require("./config/mongoose")
+require("./config/mongoose");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
