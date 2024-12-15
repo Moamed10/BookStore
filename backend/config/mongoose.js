937aea40ff626ba6b8ef93d7f6b09e09758ce380
Moamed10 <144000841+Moamed10@users.noreply.github.com>
2024-12-15 23:58:12 +0100

    a

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,9 +1,13 @@
-// const mongoose=require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-// mongoose.connect('')
-//     .then( ()=>{
-//         console.log('DB is connected')
-//     })
-//     .catch( err =>{
-//         throw err
-//     })
+const dbLink = process.env.DB_LINK;
+
+mongoose
+  .connect(dbLink)
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  });
