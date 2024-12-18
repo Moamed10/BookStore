db9b9cd41b1dd763f593e3c5e83ee944a393fbe6
Zahra <z.someh@gmail.com>
2024-12-18 11:44:32 +0100

    add all books file and display all books in home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import TopSellers from "./TopSellers";
+import AllBooks from "./AllBooks";
+
 const Home = () => {
   return (
     <>
       <Banner />
       <TopSellers />
+      <AllBooks/>
 =======
   
     </>
