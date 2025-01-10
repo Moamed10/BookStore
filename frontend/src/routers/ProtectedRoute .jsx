434873e9a48969ad4570795d47ee0212a61a9e5c
Moamed10 <144000841+Moamed10@users.noreply.github.com>
2025-01-10 14:54:57 +0100

    a

--- frontend/src/routers/ProtectedRoute .jsx
+++ frontend/src/routers/ProtectedRoute .jsx
@@ -6,19 +6,15 @@ const ProtectedRoute = ({ children, roleRequired }) => {
   const token = localStorage.getItem("token");
   const user = JSON.parse(localStorage.getItem("user"));
 
-  // If the user is not logged in, redirect to the login page
   if (!token) {
     return <Navigate to="/login" />;
   }
 
-  // If the user doesn't have the required role, redirect to the home page (or any other page)
   if (roleRequired && user.role !== roleRequired) {
     return <Navigate to="/" />;
   }
 
-  // Otherwise, render the child component
   return children;
 };
 
-// Ensure this is exported as default
 export default ProtectedRoute;
